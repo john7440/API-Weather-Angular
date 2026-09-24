fcabef70aaac5e79848ab6a8fe1d1e9b15eb63d6
Jonathan M. <neodev7440@gmail.com>
2026-09-24 06:17:44 +0200

    config: HttpClient added to the app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,6 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient()
-  ]
+  providers: [provideHttpClient()],
 };
